1d155bcdf2b847995a5feb3069a70ed6ed979172
cicerotcv <49568618+cicerotcv@users.noreply.github.com>
2019-11-13 14:25:54 -0300

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+
+function replace() {
+    // let element = document.querySelector(selector);
+    // let template = Handlebars.compile(element.innerHTML);
+    this.innerHTML = template(context);
+}
+
